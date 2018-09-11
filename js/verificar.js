70cccb653990148cffcf42e000ea64bf2f42c52a
John Molina Becker <42702401+Pandi-Chan@users.noreply.github.com>
2018-09-11 20:40:25 -0300

    Agregado Validación de Fecha

--- js/verificar.js
+++ js/verificar.js
@@ -1,33 +1,40 @@
 $(function(){
-   //Sacar los mensajes de error
-    $("error").hide();
+  //Sacar los mensajes de error
+  $("error").hide();
 
-    //Variables que indican valor de estado validacion
-	var error_email = false;
-  var error_username = false;
-	var error_password = false;
-	var error_retype_password = false;
+  //Variables que indican valor de estado validacion
+  var error_email = false;
+  var error_run = false;
+  var error_nombre = false;
+  var error_fecha = false;
+  var error_telefono = false;
+  var error_retype_password = false;
 
 
   // Cuando se Des-selecciona el Campo para el E-Mail, lo Verifica
-	$("#email").focusout(function() {
-		check_email();
-	});
+  $("#email").focusout(function() {
+    check_email();
+  });
+
+  // Cuando se Des-selecciona el Campo para el Run, lo Verifica
+  $("#run").focusout(function() {
+    check_run();
+  });
 
   // Cuando se Des-selecciona el Campo para el Nombre de Usuario, lo Verifica
   $("#nombre").focusout(function() {
-		check_name();
-	});
+    check_nombre();
+  });
 
   // Cuando se Des-selecciona el Campo para la Contraseña, lo Verifica
-	$("#passwd").focusout(function() {
-		check_password();
-	});
+  $("#fecha").focusout(function() {
+    check_fecha();
+  });
 
   // Cuando se Des-selecciona el Campo para la Confirmacion de Contraseña, lo Verifica
-	$("#repasswd").focusout(function() {
-		check_retype_password();
-	});
+  $("#telefono").focusout(function() {
+    check_telefono();
+  });
 
 
   // Funcion para Verificar el Correo
@@ -44,128 +51,220 @@ $(function(){
     }
   }
 
-  // Funcion para Verificar lel Usuario
-	function check_name() {
-		var name_length = $("#nombre").val().length;
+  // Funcion para Verificar el Run
+  function check_run() {
+    var run_length = $("#run").val().length;
+    var campo = $("#run").val();
+    if(run_length < 8 || run_length == 0) {
+      $("#errorrun").html("Largo Invalido");
+      $("#errorrun").show();
+      error_run = true;
+    } else {
+      $("#errorrun").hide();
 
-		if(name_length < 5 || name_length > 20) {
-			$("#errornombre").html("Debe tener entre 5 y 20 caracteres");
-			$("#errornombre").show();
-			error_username = true;
-		} else {
-			$("#errornombre").hide();
-		}
-	}
 
+      campo = campo.replace('-','');
+      campo = campo.replace(/\./g,'');
+
+      var suma = 0;
+      var caracteres = "1234567890kK";
+      var contador = 0;
+      for (var i=0; i < campo.length; i++){
+        u = campo.substring(i, i + 1);
+        if (caracteres.indexOf(u) != -1)
+        contador ++;
+      }
+      if ( contador==0 ) {
+        $("#errorrun").html("No se admiten letras distintas a K");
+        $("#errorrun").show();
+        error_run = true;
+      } else {
+        $("#errorrun").hide();
+
+
+        var rut = campo.substring(0,campo.length-1)
+        var drut = campo.substring( campo.length-1 )
+        var dvr = '0';
+        var mul = 2;
+
+        for (i= rut.length -1 ; i >= 0; i--) {
+          suma = suma + rut.charAt(i) * mul
+          if (mul == 7){
+            mul = 2
+          } else	{
+            mul++
+          }
+        }
+        res = suma % 11
+        if (res==1)	{
+          dvr = 'k'
+        }else if (res==0) {
+          dvr = '0'
+        }else {
+          dvi = 11-res
+          dvr = dvi + ""
+        }
+        if ( dvr != drut.toLowerCase() ) {
+          $("#errorrun").html("Run Invalido");
+          $("#errorrun").show();
+          error_run = true;
+        }else {
+          $("#errorrun").hide();
+        }
+      }
+    }
+  }
+
+  // Funcion para Verificar el Nombre
+  function check_nombre() {
+    var name_length = $("#nombre").val().length;
+
+    if(name_length < 4) {
+      $("#errornombre").html("Ingrese su Nombre");
+      $("#errornombre").show();
+      error_nombre = true;
+    } else {
+      $("#errornombre").hide();
+    }
+  }
+
+  // Funcion para Verificar la fecha
+  function check_fecha(){
+    var fecha = $("#fecha").val();
+    $("#errorfecha").html(fecha);
+    $("#errorfecha").show();
+    error_fecha = true;
+
+    if(fecha == ""){
+      $("#errorfecha").html("Ingrese Fecha de Nacimiento");
+      $("#errorfecha").show();
+      error_fecha = true;
+    } else {
+      $("#errorfecha").hide();
+
+      var anio = fecha.substring(0,4);
+      if(parseInt(anio) > parseInt("2001")){
+        $("#errorfecha").html("Menor de Edad");
+        $("#errorfecha").show();
+        error_fecha = true;
+      }else {
+        $("#errorfecha").hide();
+      }
+    }
+  }
+
+  // Funcion para Verificar el Telefono
+  function check_telefono(){
+    var campo = $("#tele").val();
+    var num = "1234567890";
+    var contador = 0;
+    for (var i=0; i < campo.length; i++){
+      u = campo.substring(i, i + 1);
+      if (caracteres.indexOf(u) != -1)
+      contador ++;
+    }
+    if ( contador==0 ) {
+      $("#errorrun").html("No se admiten letras");
+      $("#errorrun").show();
+      error_run = true;
+    } else {
+      $("#errorrun").hide();
+    }
+  }
   // Funcion para Verificar la Contraseña
   function check_password() {
-  	var password_length = $("#passwd").val().length;
+    var password_length = $("#passwd").val().length;
 
-  	if(password_length < 8) {
-  		$("#errorpasswd").html("Minimo 8 caracteres");
-  		$("#errorpasswd").show();
-  		error_password = true;
-  	} else {
-  		$("#errorpasswd").hide();
-  	}
-	}
-
-  // Funcion para Verificar la Contraseña
-	function check_retype_password() {
-
-		var password = $("#passwd").val();
-		var retype_password = $("#repasswd").val();
-
-		if(password !=  retype_password) {
-			$("#errorrepasswd").html("Contraseñas no coinciden");
-			$("#errorrepasswd").show();
-			error_retype_password = true;
-		} else {
-			$("#errorrepasswd").hide();
-		}
-	}
+    if(password_length < 8) {
+      $("#errorpasswd").html("Minimo 8 caracteres");
+      $("#errorpasswd").show();
+      error_password = true;
+    } else {
+      $("#errorpasswd").hide();
+    }
+  }
 
 
 
   // Funcion para Verificar todo al Momento de Presionar el Botón
-	$("#registration_form").submit(function() {
-		error_username = false;
-		error_password = false;
-		error_retype_password = false;
-		error_email = false;
+  $("#registration_form").submit(function() {
+    error_username = false;
+    error_password = false;
+    error_retype_password = false;
+    error_email = false;
 
-		check_username();
-		check_password();
-		check_retype_password();
-		check_email();
+    check_username();
+    check_password();
+    check_retype_password();
+    check_email();
 
-		if(error_username == false && error_password == false && error_retype_password == false && error_email == false) {
-			return true;
-		} else {
-			return false;
-		}
-	});
-	
-	function check_run() {
-		var run_length = $("#run").val().length;
-		var campo = $("#run").val();
-		if(run_length < 8 || run_length == 0) {
-			$("#errorrun").html("Largo Invalido");
-			$("#errorrun").show();
-			error_run = true;
-		} else {
-			$("#errorrun").hide();
-		
-		
-			campo = campo.replace('-','');
-			campo = campo.replace(/\./g,'');
+    if(error_username == false && error_password == false && error_retype_password == false && error_email == false) {
+      return true;
+    } else {
+      return false;
+    }
+  });
 
-			var suma = 0;
-			var caracteres = "1234567890kK";
-			var contador = 0;    
-			for (var i=0; i < campo.length; i++){
-				u = campo.substring(i, i + 1);
-				if (caracteres.indexOf(u) != -1)
-				contador ++;
-			}
-			if ( contador==0 ) { 			
-				$("#errorrun").html("No se admiten letras distintas a K");
-				$("#errorrun").show();
-				error_run = true;
-			} else {
-				$("#errorrun").hide();
-			
-			
-			var rut = campo.substring(0,campo.length-1)
-			var drut = campo.substring( campo.length-1 )
-			var dvr = '0';
-			var mul = 2;
-			
-			for (i= rut.length -1 ; i >= 0; i--) {
-				suma = suma + rut.charAt(i) * mul
-				if (mul == 7){
-					mul = 2
-				} else	{
-					mul++
-				}
-			}
-			res = suma % 11
-			if (res==1)	{
-				dvr = 'k'
-			}else if (res==0) {
-				dvr = '0'
-			}else {
-				dvi = 11-res
-				dvr = dvi + ""
-			}
-			if ( dvr != drut.toLowerCase() ) {
-				$("#errorrun").html("Run Invalido");
-				$("#errorrun").show();
-				error_run = true;
-			}else { 
-				$("#errorrun").hide();
-			}
-		}
-	}
-}
+  function check_run() {
+    var run_length = $("#run").val().length;
+    var campo = $("#run").val();
+    if(run_length < 8 || run_length == 0) {
+      $("#errorrun").html("Largo Invalido");
+      $("#errorrun").show();
+      error_run = true;
+    } else {
+      $("#errorrun").hide();
+
+
+      campo = campo.replace('-','');
+      campo = campo.replace(/\./g,'');
+
+      var suma = 0;
+      var caracteres = "1234567890kK";
+      var contador = 0;
+      for (var i=0; i < campo.length; i++){
+        u = campo.substring(i, i + 1);
+        if (caracteres.indexOf(u) != -1)
+        contador ++;
+      }
+      if ( contador==0 ) {
+        $("#errorrun").html("No se admiten letras distintas a K");
+        $("#errorrun").show();
+        error_run = true;
+      } else {
+        $("#errorrun").hide();
+
+
+        var rut = campo.substring(0,campo.length-1)
+        var drut = campo.substring( campo.length-1 )
+        var dvr = '0';
+        var mul = 2;
+
+        for (i= rut.length -1 ; i >= 0; i--) {
+          suma = suma + rut.charAt(i) * mul
+          if (mul == 7){
+            mul = 2
+          } else	{
+            mul++
+          }
+        }
+        res = suma % 11
+        if (res==1)	{
+          dvr = 'k'
+        }else if (res==0) {
+          dvr = '0'
+        }else {
+          dvi = 11-res
+          dvr = dvi + ""
+        }
+        if ( dvr != drut.toLowerCase() ) {
+          $("#errorrun").html("Run Invalido");
+          $("#errorrun").show();
+          error_run = true;
+        }else {
+          $("#errorrun").hide();
+        }
+      }
+    }
+  }
 });
